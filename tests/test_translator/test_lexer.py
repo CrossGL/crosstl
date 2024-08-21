8af0007fd48108fdca87f7423a7aa25b768f3a9a
Raghav-2903 <raghavkrishnamurthy292003@gmail.com>
2024-08-21 23:33:05 +0530

    Addressed review comments: updated lexer for bitwise operators

--- tests/test_translator/test_lexer.py
+++ tests/test_translator/test_lexer.py
@@ -99,3 +99,71 @@ def test_function_call_tokenization():
         tokenize_code(code)
     except SyntaxError:
         pytest.fail("Function call tokenization not implemented.")
+
+
+# New tests for the additional tokens
+
+def test_bitwise_operators_tokenization():
+    code = """
+    int a = 5 & 3;
+    int b = 5 | 3;
+    int c = 5 ^ 3;
+    int d = ~5;
+    int e = 5 << 1;
+    int f = 5 >> 1;
+    """
+    tokens = tokenize_code(code)
+    expected_tokens = [
+        ('INT', 'int'), ('IDENTIFIER', 'a'), ('EQUALS', '='),
+        ('NUMBER', '5'), ('BITWISE_AND', '&'), ('NUMBER', '3'), ('SEMICOLON', ';'),
+        ('INT', 'int'), ('IDENTIFIER', 'b'), ('EQUALS', '='),
+        ('NUMBER', '5'), ('BITWISE_OR', '|'), ('NUMBER', '3'), ('SEMICOLON', ';'),
+        ('INT', 'int'), ('IDENTIFIER', 'c'), ('EQUALS', '='),
+        ('NUMBER', '5'), ('BITWISE_XOR', '^'), ('NUMBER', '3'), ('SEMICOLON', ';'),
+        ('INT', 'int'), ('IDENTIFIER', 'd'), ('EQUALS', '='),
+        ('BITWISE_NOT', '~'), ('NUMBER', '5'), ('SEMICOLON', ';'),
+        ('INT', 'int'), ('IDENTIFIER', 'e'), ('EQUALS', '='),
+        ('NUMBER', '5'), ('BITWISE_SHIFT_LEFT', '<<'), ('NUMBER', '1'), ('SEMICOLON', ';'),
+        ('INT', 'int'), ('IDENTIFIER', 'f'), ('EQUALS', '='),
+        ('NUMBER', '5'), ('BITWISE_SHIFT_RIGHT', '>>'), ('NUMBER', '1'), ('SEMICOLON', ';')
+    ]
+    assert tokens == expected_tokens
+
+
+def test_assignment_operators_tokenization():
+    code = """
+    int a = 5;
+    a &= 3;
+    a |= 3;
+    a ^= 3;
+    a %= 3;
+    a <<= 1;
+    a >>= 1;
+    """
+    tokens = tokenize_code(code)
+    expected_tokens = [
+        ('INT', 'int'), ('IDENTIFIER', 'a'), ('EQUALS', '='), ('NUMBER', '5'), ('SEMICOLON', ';'),
+        ('IDENTIFIER', 'a'), ('ASSIGN_AND', '&='), ('NUMBER', '3'), ('SEMICOLON', ';'),
+        ('IDENTIFIER', 'a'), ('ASSIGN_OR', '|='), ('NUMBER', '3'), ('SEMICOLON', ';'),
+        ('IDENTIFIER', 'a'), ('ASSIGN_XOR', '^='), ('NUMBER', '3'), ('SEMICOLON', ';'),
+        ('IDENTIFIER', 'a'), ('ASSIGN_MOD', '%='), ('NUMBER', '3'), ('SEMICOLON', ';'),
+        ('IDENTIFIER', 'a'), ('ASSIGN_SHIFT_LEFT', '<<='), ('NUMBER', '1'), ('SEMICOLON', ';'),
+        ('IDENTIFIER', 'a'), ('ASSIGN_SHIFT_RIGHT', '>>='), ('NUMBER', '1'), ('SEMICOLON', ';')
+    ]
+    assert tokens == expected_tokens
+
+
+def test_data_types_and_const_tokenization():
+    code = """
+    const double pi = 3.14159;
+    unsigned int maxVal = 255;
+    """
+    tokens = tokenize_code(code)
+    expected_tokens = [
+        ('CONST', 'const'), ('DOUBLE', 'double'), ('IDENTIFIER', 'pi'), ('EQUALS', '='),
+        ('FLOAT_NUMBER', '3.14159'), ('SEMICOLON', ';'),
+        ('UNSIGNED_INT', 'unsigned int'), ('IDENTIFIER', 'maxVal'), ('EQUALS', '='),
+        ('NUMBER', '255'), ('SEMICOLON', ';')
+    ]
+    assert tokens == expected_tokens
+
