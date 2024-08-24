dd0cf7ad303dd1a552f9e23e5975192c7f58d845
Sahil06012002 <94986140+Sahil06012002@users.noreply.github.com>
2024-08-24 17:15:42 +0530

    updated parser class in tranlator module to handle logical operators (#94)
    * updated parser class in tranlator module to handle logical operators
    
    * updated test case
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: Sahil Ahmad <sahmad@EX2INDIA.LOCAL>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/test_translator/test_parser.py
+++ tests/test_translator/test_parser.py
@@ -207,3 +207,43 @@ def test_function_call():
         parse_code(tokens)
     except SyntaxError:
         pytest.fail("Struct parsing not implemented.")
+
+
+def test_logical_operators():
+    code = """
+        shader LightControl {
+        vertex {
+            input vec3 position;
+            output float isLightOn;
+
+            void main() {
+                if (position.x > 0.3 && position.z < 0.7) {
+                    isLightOn = 1.0;
+                } else {
+                    isLightOn = 0.0;
+                }
+
+                // Set the vertex position
+                gl_Position = vec4(position, 1.0);
+            }
+        }
+
+        fragment {
+            input float isLightOn;
+            output vec4 fragColor;
+
+            void main() {
+                if (isLightOn == 1.0) {
+                    fragColor = vec4(1.0, 1.0, 0.0, 1.0);
+                } else {
+                    fragColor = vec4(0.0, 0.0, 0.0, 1.0);
+                }
+            }
+        }
+    }
+    """
+    try:
+        tokens = tokenize_code(code)
+        parse_code(tokens)
+    except SyntaxError:
+        pytest.fail("Struct parsing not implemented.")
