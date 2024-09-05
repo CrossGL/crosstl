f84b2eba61bf0de70a8b5728f92d4f52776c0389
shankkyy <shivv2403@gmail.com>
2024-09-05 20:30:17 +0530

    added assignment shift right to translator support metal_codegen.py

--- crosstl/src/translator/codegen/metal_codegen.py
+++ crosstl/src/translator/codegen/metal_codegen.py
@@ -427,5 +427,6 @@ class MetalCodeGen:
             "AND": "&&",
             "OR": "||",
             "EQUALS": "=",
+            "ASSIGN_SHIFT_RIGHT": ">>=",
         }
         return op_map.get(op, op)
