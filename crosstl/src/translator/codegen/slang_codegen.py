6fcacf50ccfd0f30c8fd2a916c72cbbc851373ed
Nripesh Niketan <nripesh14@gmail.com>
2024-11-26 09:59:35 +0000

    Implement initial structure for SlangCodeGen class with constructor and generate method

--- crosstl/src/translator/codegen/slang_codegen.py
+++ crosstl/src/translator/codegen/slang_codegen.py
@@ -15,4 +15,9 @@ from ..ast import (
 
 # ToDO: Implement the SlangCodeGen class
 class SlangCodeGen:
-    pass
+    def __init__(self):
+        pass
+    
+    def generate(self, ast):
+        pass
+
