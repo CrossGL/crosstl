591bd890055747ddd059775e560dd2456d1a4dbe
Nripesh Niketan <86844847+NripeshN@users.noreply.github.com>
2024-11-26 10:07:12 +0000

    Chore: add Slang translator template for frontend & tests (#211)
    * enh(tests): Update component list in translator-tests.yml to include 'slang'
    
    * enh(tests): Add 'slang' to backend list in backend-tests.yml
    
    * Todo task later add slang test
    
    * Add slang codegen template and test template
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Chore: add slang codegen test template
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Implement initial structure for SlangCodeGen class with constructor and generate method
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Remove unwanted comment
    
    ---------

---
+++ crosstl/src/translator/codegen/slang_codegen.py
@@ -0,0 +1,22 @@
+from ..ast import (
+    AssignmentNode,
+    BinaryOpNode,
+    ForNode,
+    FunctionCallNode,
+    IfNode,
+    MemberAccessNode,
+    ReturnNode,
+    StructNode,
+    TernaryOpNode,
+    UnaryOpNode,
+    VariableNode,
+)
+
+
+# ToDO: Implement the SlangCodeGen class
+class SlangCodeGen:
+    def __init__(self):
+        pass
+
+    def generate(self, ast):
+        pass
---
+++ tests/test_translator/test_codegen/test_slang_codegen.py
@@ -0,0 +1,43 @@
+from crosstl.src.translator.lexer import Lexer
+import pytest
+from typing import List
+from crosstl.src.translator.parser import Parser
+from crosstl.src.translator.codegen.slang_codegen import SlangCodeGen
+
+
+def tokenize_code(code: str) -> List:
+    """Helper function to tokenize code."""
+    lexer = Lexer(code)
+    return lexer.tokens
+
+
+def parse_code(tokens: List):
+    """Helper function to parse tokens into an AST.
+
+    Args:
+        tokens (List): The list of tokens to parse
+    Returns:
+        AST: The abstract syntax tree generated from the parser
+    """
+    parser = Parser(tokens)
+    return parser.parse()
+
+
+def generate_code(ast_node):
+    """Test the code generator
+    Args:
+        ast_node: The abstract syntax tree generated from the code
+    Returns:
+        str: The generated code from the abstract syntax tree
+    """
+    codegen = SlangCodeGen()
+    return codegen.generate(ast_node)
+
+
+# ToDO: Implement the tests
+def test_struct():
+    pass
+
+
+if __name__ == "__main__":
+    pytest.main()
