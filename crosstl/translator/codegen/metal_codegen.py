4dec777b21c08dfb07a867f3dba1dddabcd72098
Jyotin Goel <120490013+gjyotin305@users.noreply.github.com>
2024-12-27 01:18:28 +0530

    Fix repeated key assign_xor (#235)
    Signed-off-by: Jyotin Goel <b22ai063@iitj.ac.in>

--- crosstl/translator/codegen/metal_codegen.py
+++ crosstl/translator/codegen/metal_codegen.py
@@ -365,7 +365,6 @@ class MetalCodeGen:
             "ASSIGN_SHIFT_RIGHT": ">>=",
             "ASSIGN_AND": "&=",
             "LOGICAL_AND": "&&",
-            "ASSIGN_XOR": "^=",
             "BITWISE_SHIFT_RIGHT": ">>",
             "BITWISE_SHIFT_LEFT": "<<",
         }
