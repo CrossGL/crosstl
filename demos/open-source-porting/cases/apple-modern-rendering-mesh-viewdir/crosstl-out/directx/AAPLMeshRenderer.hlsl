d0351951bd4485443fa577931fc23c6b758601a9
Nripesh Niketan <nripesh14@gmail.com>
2026-06-10 14:11:20 +0100

    Refresh Apple demo after main merge

--- demos/open-source-porting/cases/apple-modern-rendering-mesh-viewdir/crosstl-out/directx/AAPLMeshRenderer.hlsl
+++ demos/open-source-porting/cases/apple-modern-rendering-mesh-viewdir/crosstl-out/directx/AAPLMeshRenderer.hlsl
@@ -1,6 +1,6 @@
 
 struct Camera {
-    float4x4 invViewMatrix;
+    float4x4 invViewMatrix: TEXCOORD0;
 };
 struct Input {
     float3 position: POSITION;
@@ -9,6 +9,6 @@ struct Output {
     half3 viewDir: TEXCOORD0;
 };
 // Vertex Shader
-Output VSMain(Input in_, ReferenceType(referenced_type=NamedType(name=Camera, generic_args= camera[]), is_mutable=False)) {
+Output VSMain(Input in_, ConstantBuffer<Camera> camera) {
     return Output(half3(0));
 }
